0cd18a4aabace547358a5d9d3fa95d92609073f6
Usuario <stevinterlerez@gmail.com>
2019-12-13 17:57:00 +0100

    Terecera activity Descartada la idea de un input layout, demasiado complejo de emplear para mi al menos(poca info en internet sobre su manejo, al menos en kotlin, no me funciona ningun tipo de getText()) Probable cambio a una checkbox, o a otro sistema para chequear las opciones FInalamente me decanto por la check, puesto que facilita en demasia la complejidad del programa Finalizado la parte tres Comienzo de la cuarta y ultima activity

--- app/src/main/java/com/example/retosbotones/retoSuma.kt
+++ app/src/main/java/com/example/retosbotones/retoSuma.kt
@@ -1,11 +1,14 @@
 package com.example.retosbotones
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_reto_camara.*
 import kotlinx.android.synthetic.main.activity_reto_suma.*
 import kotlinx.android.synthetic.main.activity_reto_suma.btnSalir
+import org.jetbrains.anko.toast
 
 class retoSuma : AppCompatActivity() {
 
@@ -18,10 +21,24 @@ class retoSuma : AppCompatActivity() {
 
 
     }
+    //SE INDICA MEDIANTE CHECKBOX(LA MANERA MAS SENCILLA QUE ENCONTRE PARA COMPLETAR ESTE RETO)
+    //TIENES DOS OPCIONES; AL MARCAR UNA CHECK SE CONVIERTE EN TRUE(LAS CHECK SON BOOLEAN)
+    //SI LA CORRECTA ES TRUE Y LA OTRA ES FALSE SE DA POR BUENO
     fun comprobarText(){
+        if(opcion1.isChecked==false&&opcion2.isChecked==true){
+            toast("Acierto")
+
+            val inf = 1
+            val data = Intent()
+
+            data.putExtra("inf", inf)
+            setResult(Activity.RESULT_OK,data)
+        }
 
 
 
             }
+    fun salir(){
+        finish()
     }
 }
